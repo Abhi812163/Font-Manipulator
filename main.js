50569ad023181dbb833f30862dc2a3252feed4c4
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-08-10 19:26:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(900,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#adadad');
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}  
