72c682bda636683bdff4f4ea4622b2cef8330136
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-08-12 15:20:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -9,6 +15,10 @@ function setup(){
 
 function draw(){
     background('#adadad');
+    text(difference)
+    fill('#00ff3c');
+    text('Abhineet',50,400);
+    document.getElementById("size").innerHTML="Width and Height of the text is = "+ difference+"px";
 }
 
 function modelLoaded(){
@@ -17,6 +27,9 @@ function modelLoaded(){
 
 function gotPoses(results){
     if(results.length>0){
-        console.log(results);
-    }
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX - rightWristX);
+        console.log("leftWrist x= "+leftWristX + " rightWrist x = " + rightWristX + " difference= " + difference);
 }  
+}
